ccdc696ba76b4e4081ff120e9066d0eace2390f4
hyeonseol00 <hyeonseol22@naver.com>
2024-05-01 17:14:52 +0900

    ✨ update  : review object get & set 함수 추가

--- js/common-local-storage.js
+++ js/common-local-storage.js
@@ -1,6 +1,6 @@
 function makeReviewObjectInLocalStorage(movieId) {	
 	let tempObject;
-	
+
 	if (localStorage.review === undefined) 
 		tempObject = { review: {} };
 	else
@@ -11,4 +11,12 @@ function makeReviewObjectInLocalStorage(movieId) {
 	localStorage.review = JSON.stringify(tempObject);
 }
 
-export { makeReviewObjectInLocalStorage };
+function getReviewObject() {
+	return JSON.parse(localStorage.review);
+}
+
+function setReviewObject(obj) {
+	localStorage.review = JSON.stringify(obj);
+}
+
+export { makeReviewObjectInLocalStorage, getReviewObject, setReviewObject };
