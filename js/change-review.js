db6ca6ea5994e7563c35c177b5c1a4fc10d3ddc2
hyeonseol00 <hyeonseol22@naver.com>
2024-05-01 17:42:08 +0900

    ✨ update  : 리뷰 수정 함수 구현 완료

--- js/change-review.js
+++ js/change-review.js
@@ -1,16 +1,14 @@
-localStorage.review = JSON.stringify({
-	278: [{
-		name: "재석", password: "1234", text: "리뷰 내용"
-	}, {
-		name: "민도", password: "1543", text: "리뷰 내용"
-	}],
-	372: [{
-		name: "재석", password: "1234", text: "리뷰 내용"
-	}, {
-		name: "민도", password: "1543", text: "리뷰 내용"
-	}]
-});
-let tempObject = JSON.parse( localStorage.review );
+import { getReviewObject, setReviewObject } from "./common-local-storage.js";
 
-// tempObject를 사용해서 데이터베이스 수정 후 다시 localStorage에 적용시킬 때
-localStorage.review = JSON.stringify(tempObject);
+function changeReview(movieId, idx, name, password, text) {
+	let tempObject = getReviewObject();
+
+	if (password == tempObject[movieId][idx].password) {
+		tempObject[movieId][idx].name = name;
+		tempObject[movieId][idx].text = text;
+	}
+
+	setReviewObject(tempObject);
+}
+
+export { changeReview };
