e7f37085a7d4ed0395eabf567dbca0d4b72eb3a9
hyeonseol00 <hyeonseol22@naver.com>
2024-05-02 11:55:48 +0900

    ✨ update : 정보창 쿼리 이벤트 등록 함수, 메인 페이지로 돌아가는 함수 생성

--- js/switch-info-page.js
+++ js/switch-info-page.js
@@ -1,10 +1,28 @@
+import { addEventListeners, initializeQuerySelector, loadPaginationButtonState, makeCards } from "./common.js";
+
 async function fetchHtmlAsText(url) {
 	return await (await fetch(url)).text();
 }
 
 async function importPage(target) {
 	document.querySelector('body').innerHTML = await fetchHtmlAsText(target);
-	console.log("clear import html");
 }
 
-export { importPage };
+function initInformationPage() {
+	let $exitButton = document.querySelector("#exit-button");
+
+	$exitButton.addEventListener('click', (event) => {
+		event.preventDefault();
+		backToMainPage();
+	});
+}
+
+async function backToMainPage() {
+	await importPage("html/main.html");
+	initializeQuerySelector();
+	makeCards("");
+	addEventListeners();
+	loadPaginationButtonState();
+}
+
+export { importPage, initInformationPage };
