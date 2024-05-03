e0c62d78225dd91645f7879195e1a82e219eed99
황민도 <167045161+hwangmindo@users.noreply.github.com>
2024-05-03 15:50:48 +0900

    🩹 fix: input파일 들여쓰기 수정

--- js/input-review.js
+++ js/input-review.js
@@ -3,58 +3,58 @@ import { getReviewObject, setReviewObject } from "./common-local-storage.js";
 
 // 해당 html id를 통해 value 값을 가져옴
 function inputReview(inputId) { //idInput
-    const $inputId = document.getElementById(inputId);
-    const madevalue = $inputId.value;//input 내용
-    if (madevalue.trim() !== "") {
-        return madevalue;
-    } else {
-        return "empty8792";
-    };
+	const $inputId = document.getElementById(inputId);
+	const madevalue = $inputId.value;//input 내용
+	if (madevalue.trim() !== "") {
+		return madevalue;
+	} else {
+		return "empty8792";
+	};
 }
 
 // 입력내용=>객체 생성 로직
 function madeObject(secondinputId) {
-    let catchError = 0
+	let catchError = 0
 	const firstinputId = ["id", "pw", "text"]; //firstId 값\ reset 가능 but, 하위 변수도 동일 개수로 수정
-    const objectKey = ["name", "password", "text"]; //key 값\ reset 가능 but, 상위 변수도 동일 개수로 수정
-    let pullinputId = []; //idInput, ...
-    let answerObject = {};//name: "input", ...
-    firstinputId.forEach((first) => {
+	const objectKey = ["name", "password", "text"]; //key 값\ reset 가능 but, 상위 변수도 동일 개수로 수정
+	let pullinputId = []; //idInput, ...
+	let answerObject = {};//name: "input", ...
+	firstinputId.forEach((first) => {
 		pullinputId.push(first + secondinputId); //결과 : idInput
 	});
-    for (let i = 0; i < firstinputId.length; i++) {
-        let pull = pullinputId[i];
-        answerObject[objectKey[i]] = inputReview(pull);
-    };
-    for (let j = 0; j < firstinputId.length; j++) {
-        let catchValue = answerObject[objectKey[j]]
-        if (catchValue === "empty8792") {
-            alert(`${objectKey[j]} 입력해주세요.`)
-            catchError = 8792
-            break
-        }
-    };
-    if (catchError === 8792) {
-        return 8792
-    } else {
-        for (let i = 0; i < pullinputId.length; i++) {             //input 초기화 새로 기입
-            document.getElementById(pullinputId[i]).value = '';    //input 초기화 새로 기입
-        };
-        return answerObject //name: "input", ...
-    };
+	for (let i = 0; i < firstinputId.length; i++) {
+		let pull = pullinputId[i];
+		answerObject[objectKey[i]] = inputReview(pull);
+	};
+	for (let j = 0; j < firstinputId.length; j++) {
+		let catchValue = answerObject[objectKey[j]]
+		if (catchValue === "empty8792") {
+			alert(`${objectKey[j]} 입력해주세요.`)
+			catchError = 8792
+			break
+		}
+	};
+	if (catchError === 8792) {
+		return 8792
+	} else {
+		for (let i = 0; i < pullinputId.length; i++) {             //input 초기화 새로 기입
+			document.getElementById(pullinputId[i]).value = '';    //input 초기화 새로 기입
+		};
+		return answerObject //name: "input", ...
+	};
 }
 
 // localstorage로 객체내용 update 하는 로직
 function storageUpdate(movieId, secondId) {
-    let tempObject = getReviewObject();
-    let userObject = madeObject(secondId);
-    if (userObject === 8792) {
-        setReviewObject(tempObject)
-    } else {
-        tempObject[movieId].push(userObject); // 234: [{name: "input", ...}]
-        setReviewObject(tempObject);
-        alert("리뷰가 등록되었습니다.") //리뷰등록안내 새로 기입
-    };
+	let tempObject = getReviewObject();
+	let userObject = madeObject(secondId);
+	if (userObject === 8792) {
+		setReviewObject(tempObject)
+	} else {
+		tempObject[movieId].push(userObject); // 234: [{name: "input", ...}]
+		setReviewObject(tempObject);
+		alert("리뷰가 등록되었습니다.") //리뷰등록안내 새로 기입
+	};
 }
 
 // <등록 버튼>
@@ -62,26 +62,26 @@ function storageUpdate(movieId, secondId) {
 //seconId 정의 : html input태그의 변형되는 firstId 는 생략하고 그 나머지 반복되는 Id
 // Ex: updateBtr(movieId, "updatebtr", "input") 이렇게 설정 가능
 function uploadBtr(movieId, brtId, secondId) {
-    const $getbrt = document.getElementById(brtId);
-    $getbrt.addEventListener("click", (event) => {
-        storageUpdate(movieId, secondId);
-        temping(movieId, "rivew-borderbox");
-    });
+	const $getbrt = document.getElementById(brtId);
+	$getbrt.addEventListener("click", (event) => {
+		storageUpdate(movieId, secondId);
+		temping(movieId, "rivew-borderbox");
+	});
 }
 
 
 // 리뷰탬핑로직
 function temping(movieId, tagId) {
-    const $getTagId = document.getElementById(tagId);
-    let openObject = getReviewObject();
-    if (!openObject[movieId] || openObject[movieId].length === 0) {
-        $getTagId.innerHTML = ``;
-        return;
-    };
-    $getTagId.innerHTML = ``;
-    let i = 0;
-    openObject[movieId].forEach((getObject) => {
-        let temp_rivew = `
+	const $getTagId = document.getElementById(tagId);
+	let openObject = getReviewObject();
+	if (!openObject[movieId] || openObject[movieId].length === 0) {
+		$getTagId.innerHTML = ``;
+		return;
+	};
+	$getTagId.innerHTML = ``;
+	let i = 0;
+	openObject[movieId].forEach((getObject) => {
+		let temp_rivew = `
         <div class="card border-secondary mb-3" data-idx="${i}" data-password="${getObject["password"]}">
             <div class="card-header">
                 <h5>${getObject["name"]}</h5>
@@ -95,10 +95,10 @@ function temping(movieId, tagId) {
             </div>
         </div>
         `;
-        $getTagId.insertAdjacentHTML('beforeend', temp_rivew);
+		$getTagId.insertAdjacentHTML('beforeend', temp_rivew);
 		addUpdateDeleteEvent(i);
-        i++;
-    });
+		i++;
+	});
 }
 
 function addUpdateDeleteEvent(idx) {
